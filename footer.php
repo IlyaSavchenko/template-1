fc66c0a67cfd6b9664c0985cc468d8233f838347
IlyaSavchenko <sav.il.yu@gmail.com>
2018-12-04 14:23:35 +0300

    initial commit

---
+++ footer.php
@@ -0,0 +1,54 @@
+
+	<ul class="footer-menu">
+	    <li>
+	        <?php if (is_page('o-proekte')){?>
+	            <span class="item-footer-menu-span">О проекте</span>
+	        <?php } else {?>
+	            <a href="//svekrovi.net/o-proekte"  rel="nofollow" class="item-footer-menu">О проекте</a>
+	        <?php }?>
+	    </li>
+	    <li>
+	        <?php if (is_page('vopros-ekspertu')){?>
+	            <span class="item-footer-menu-span">Вопрос эксперту</span>
+	        <?php } else {?>
+	            <a href="//svekrovi.net/vopros-ekspertu" rel="nofollow" class="item-footer-menu">Вопрос эксперту</a>
+	            <?php }?>
+	    </li>
+	    <li>
+	        <?php if (is_page('kontakty')){?>
+	            <span class="item-footer-menu-span">Контакты</span>
+	        <?php } else {?>
+	            <a href="//svekrovi.net/kontakty" rel="nofollow" class="item-footer-menu">Контакты</a>
+	        <?php }?>
+	    </li>
+	    <li>
+	        <?php if (is_page('reklama')){?>
+	            <span class="item-footer-menu-span">Реклама</span>
+	        <?php } else {?>
+	            <a href="//svekrovi.net/reklama" rel="nofollow" class="item-footer-menu">Реклама</a>
+	        <?php }?>
+	    </li>
+	    <li>
+	        <?php if (is_page('spravochnik')){?>
+	            <span class="item-footer-menu-span">Справочник</span>
+	        <?php } else {?>
+	            <a href="//svekrovi.net/spravochnik" rel="nofollow" class="item-footer-menu">Справочник</a>
+	        <?php }?>
+	    </li>
+	    <li>
+	        <?php if (is_page('karta-sajta')){?>
+	            <span class="item-footer-menu-span">Карта сайта</span>
+	        <?php } else {?>
+	            <a href="//svekrovi.net/karta-sajta" rel="nofollow" class="item-footer-menu">Карта сайта</a>
+	        <?php }?>
+	    </li>
+	    <li>
+	        <?php if (is_page('polzovatelskoe-soglashenie')){?>
+	                <span class="item-footer-menu-span">Пользовательское соглашение</span>
+            <?php } else {?>
+                <a href="//svekrovi.net/polzovatelskoe-soglashenie" rel="nofollow" class="item-footer-menu">Пользовательское соглашение</a>
+            <?php }?>
+	    </li>
+	</ul>
+</body>
+</html>
