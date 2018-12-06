1a3a5f26c042080005ff59ba6cf86869c3eae68c
IlyaSavchenko <sav.il.yu@gmail.com>
2018-12-06 17:26:17 +0300

    create functional for generate author url

--- single.php
+++ single.php
@@ -1,23 +1,31 @@
 <?php get_header(); ?>
 	<section id="article" class="bgc">
-        <div class="container main-container">
+        <div class="container main-container ">
             <div class="row">
-                <div class="col-12 col-md-8 content">
+                <div class="col-12 col-md-8 content-container boreder">
+                	<?php
+						if ( function_exists('yoast_breadcrumb') ) {
+						  yoast_breadcrumb( '<div id="breadcrumbs">','</div>' );
+						}
+					?>
                     <div  itemscope itemtype="http://schema.org/Article" class="single-content" >
                         <h1 itemprop = "name"><?php the_title(); ?></h1>
+                        <?php echo  get_the_author_meta( 'nickname' );
+						$author = get_the_author_meta( 'user_nicename' );
+                        echo 'http://serezhq6.beget.tech/author/'.$author;?>
                         <div itemprop="articleBody" class="text-article">
                             <?php
-                                // global $more;
-                                // while( have_posts() ) : the_post();
-                                //     $more = 1; // отображаем полностью всё содержимое поста
+                                global $more;
+                                while( have_posts() ) : the_post();
+                                    $more = 1; // отображаем полностью всё содержимое поста
                                     the_content(); // выводим контент
-                                // endwhile; 
+                                endwhile; 
                                 // wp_reset_postdata();  //удалить
                             ?>
                         </div>
                     </div>    
-            </div>
-        </div>
-    </div>
+	            </div>
+	        </div>
+	    </div>
     </section>
 <?php get_footer(); ?>
